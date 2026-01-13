ca473b9298857a9b05ab46065a70d1500e29f105
Devang Gajjar <DevangGajjar077@gmail.com>
2026-01-13 15:40:55 +0530

    initial commit

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Middleware\Authenticate as MiddlewareAuthenticate;
+
+class Authenticate extends MiddlewareAuthenticate
+{
+    /**
+     * Handle an unauthenticated user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
+     * @return never
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function unauthenticated($request, array $guards)
+    {
+        foreach ($guards as $guard) {
+            switch ($guard) {
+                case 'admin':
+                    throw new AuthenticationException(redirectTo: route('admin.login'));
+                    break;
+                default:
+                    throw new AuthenticationException(redirectTo: route('login'));
+            }
+        }
+    }
+}
---
+++ app/View/Components/AppLayout.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\View\View;
+
+class AppLayout extends Component
+{
+    /**
+     * Get the view / contents that represents the component.
+     */
+    public function render(): View
+    {
+        if (auth()->guard('admin')->check()) {
+            return view('admin.layouts.app');
+        }
+        return view('layouts.app');
+    }
+}
